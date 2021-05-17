2eb3af88bebb928712235a685dadc8376e128f6a
kakorotto <paulaatiya@gmail.com>
2021-05-18 00:40:11 +0200

    commiy

--- blog/query/index.js
+++ blog/query/index.js
@@ -6,9 +6,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const post = {};
 
-app.post("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(post);
+});
+
+app.post("/posts", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    post[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("listen on 4002");
